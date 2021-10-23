976dfa087ddb016f28471a8d4aa2568c15ca2cd5
FranciscoValenzuelaPaz <francisco.val920@gmail.com>
2021-10-23 19:23:10 -0300

    "Avance Vistas se realizo IniciarSesionActivity RegistroActivity y InicioActivity(En proceso)"

--- ProyectoAM/app/src/main/java/com/example/proyectoam/MainActivity.java
+++ ProyectoAM/app/src/main/java/com/example/proyectoam/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.proyectoam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Francisco Valenzuela
         setContentView(R.layout.activity_main);
+    }
+    public void cambiarRegistro(View view){
+        Intent intent = new Intent(this, RegistroActivity.class);
+        startActivity(intent);
     }
 }
